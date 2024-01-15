f16cd0f628ef6cc33e4fb451b55fad37f1daaa34
Anders Bjerner <abjerner@skybrud.dk>
2024-01-15 14:26:04 +0100

    Added value convert for the job ID and last updated property editors

---
+++ src/Limbo.Umbraco.Signatur/PropertyEditors/SignaturJobIdValueConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Skybrud.Essentials.Strings.Extensions;
+using Umbraco.Cms.Core.Models.PublishedContent;
+using Umbraco.Cms.Core.PropertyEditors;
+
+namespace Limbo.Umbraco.Signatur.PropertyEditors;
+
+public class SignaturJobIdValueConverter : PropertyValueConverterBase {
+
+    public override bool IsConverter(IPublishedPropertyType propertyType) {
+        return propertyType.EditorAlias == SignaturJobIdEditor.EditorAlias;
+    }
+
+    public override object? ConvertSourceToIntermediate(IPublishedElement owner, IPublishedPropertyType propertyType, object? source, bool preview) {
+        return source;
+    }
+
+    public override object? ConvertIntermediateToObject(IPublishedElement owner, IPublishedPropertyType propertyType, PropertyCacheLevel referenceCacheLevel, object? inter, bool preview) {
+        return inter switch {
+            int integer => integer,
+            string str => str.ToInt32(),
+            _ => 0
+        };
+    }
+
+    public override Type GetPropertyValueType(IPublishedPropertyType propertyType) {
+        return typeof(int);
+    }
+
+    public override PropertyCacheLevel GetPropertyCacheLevel(IPublishedPropertyType propertyType) {
+        return PropertyCacheLevel.Element;
+    }
+
+}
---
+++ src/Limbo.Umbraco.Signatur/PropertyEditors/SignaturLastUpdatedValueConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Skybrud.Essentials.Time;
+using Umbraco.Cms.Core.Models.PublishedContent;
+using Umbraco.Cms.Core.PropertyEditors;
+
+namespace Limbo.Umbraco.Signatur.PropertyEditors;
+
+public class SignaturLastUpdatedValueConverter : PropertyValueConverterBase {
+
+    public override bool IsConverter(IPublishedPropertyType propertyType) {
+        return propertyType.EditorAlias == SignaturLastUpdatedEditor.EditorAlias;
+    }
+
+    public override object? ConvertSourceToIntermediate(IPublishedElement owner, IPublishedPropertyType propertyType, object? source, bool preview) {
+        return source is string str ? str[1..] : null;
+    }
+
+    public override object? ConvertIntermediateToObject(IPublishedElement owner, IPublishedPropertyType propertyType, PropertyCacheLevel referenceCacheLevel, object? inter, bool preview) {
+        return inter is string str ? EssentialsTime.FromIso8601(str) : null;
+    }
+
+    public override Type GetPropertyValueType(IPublishedPropertyType propertyType) {
+        return typeof(EssentialsTime);
+    }
+
+    public override PropertyCacheLevel GetPropertyCacheLevel(IPublishedPropertyType propertyType) {
+        return PropertyCacheLevel.Element;
+    }
+
+}
